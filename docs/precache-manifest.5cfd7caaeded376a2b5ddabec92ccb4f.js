59a705e6b38462e1a6e1249b69dd9222a6e5b9d7
GulTion <Gkv8192@gmail.com>
2021-01-25 13:23:02 +0000

    Upload docs to statics pages

---
+++ docs/precache-manifest.5cfd7caaeded376a2b5ddabec92ccb4f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb0268950fb2687a98130730933b6400",
+    "url": "/counter/index.html"
+  },
+  {
+    "revision": "19ec690ccbd1abe5fed3",
+    "url": "/counter/static/css/main.e45098ae.chunk.css"
+  },
+  {
+    "revision": "c6a19e366fe3ae8feef4",
+    "url": "/counter/static/js/2.065046f7.chunk.js"
+  },
+  {
+    "revision": "19ec690ccbd1abe5fed3",
+    "url": "/counter/static/js/main.ffb4f637.chunk.js"
+  },
+  {
+    "revision": "91cbd575dd3fae8f0028",
+    "url": "/counter/static/js/runtime-main.c526212f.js"
+  }
+]);
